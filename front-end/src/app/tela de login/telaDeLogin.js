97e0aa3ec9ecb3be008e8e8a90901974997eb977
Miguel <miguelbraz700@gmail.com>
2025-04-11 13:09:01 -0300

    telas novas

---
+++ front-end/src/app/tela de login/telaDeLogin.js
@@ -0,0 +1,12 @@
+'use client'
+
+import LoginTela from '@/components/loginTela';
+
+export default function Home() {
+    return (
+      <main>
+        <LoginTela />
+      </main>
+    );
+  }
+  
